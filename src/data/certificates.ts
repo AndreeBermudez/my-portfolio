acb3ce0a508a86d22d3691ff19691c259515de0f
AndreeBermudez <andreebermudez1408@gmail.com>
2025-08-07 01:30:34 -0500

    Añadidas nuevas imágenes y corregidas las rutas en los archivos de proyectos y certificados

--- src/data/certificates.ts
+++ src/data/certificates.ts
@@ -3,24 +3,24 @@ export const certificates = [
 		title: '1er Puesto Hackathon',
 		organization: 'UTP+ by Xpedition',
 		date: 'Junio 2025',
-		image: '/src/assets/diploma_1ro.jpg',
+		image: '/images/diploma_1ro.jpg',
 	},
 	{
 		title: 'Firebase Day 2025',
 		organization: 'GDG Chimbote',
 		date: 'Junio 2025',
-		image: '/src/assets/gdg_chimbote.jpg',
+		image: '/images/gdg_chimbote.jpg',
 	},
 	{
 		title: 'Hackathon UTP - El Comercio',
 		organization: 'UTP & El Comercio',
 		date: 'Noviembre 2024',
-		image: '/src/assets/hackaton_comercio.jpg',
+		image: '/images/hackaton_comercio.jpg',
 	},
 	{
 		title: 'Git y Github',
 		organization: 'Alura Latam',
 		date: 'Enero 2024',
-		image: '/src/assets/alura.jpg',
+		image: '/images/alura.jpg',
 	},
 ];
